6e9463dc7619796b4cb7229dc08f21a9dee8caa0
fom78 <fom78a@gmail.com>
2021-02-07 02:19:13 -0300

    Iniciando deploy, archivos estaticos en backend.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-// fetch('http://localhost:3001/notes')
-//   .then(response => response.json())
-//   .then(data => console.log('Desde el fetch: ',data))
-
   ReactDOM.render(
     <App />,
     document.getElementById('root')
--- src/services/notes.js
+++ src/services/notes.js
@@ -1,11 +1,7 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
-
-
-// const getAll = () => {
-//     const request = axios.get(baseUrl)
-//     return request.then(response => response.data)
-//   }
+//const baseUrl = 'http://localhost:3001/api/notes'
+//const baseUrl = 'https://mysterious-dawn-46523.herokuapp.com/api/notes'
+const baseUrl = '/api/notes'
 const getAll = () => {
     const request = axios.get(baseUrl)
     const nonExisting = {
